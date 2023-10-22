b1400158aab5c2c5e8af7faf852d0ccc71b43cc2
dlucianop <3112dlp@gmail.com>
2023-10-22 10:16:15 -0600

    Modificaciones minimas

--- js/index.js
+++ js/index.js
@@ -1,20 +1,44 @@
 var num_pacientes = document.getElementsByClassName("paciente").length;
+//document.querySelectorAll("paciente").length;
 
 function imc(peso, altura) {
     return (peso / (altura * altura)).toFixed(2);
 }
 
+function validationPeso(peso) {
+    // para signo OR se usa las teclas ALT+124
+    return (peso > 0 && peso < 1000)
+}
+
+function validationAltura(altura) {
+    return (altura > 0 && altura < 3)
+}
+
 function getPaciente() {
-    for (i = 1; i < num_pacientes+1; i++){
+    for (i = 1; i <= num_pacientes; i++){
         var paciente = document.getElementById(i);
+        var nombre = paciente.querySelector(".info-nombre").textContent;
         var peso = paciente.querySelector(".info-peso").textContent;
         var altura = paciente.querySelector(".info-altura").textContent;
 
-        var imcPaciente = imc(parseFloat(peso), parseFloat(altura))
-        console.log(imcPaciente)
+        var verificacionPeso = validationPeso(parseFloat(peso));
+        var verificacionAltura = validationAltura(parseFloat(altura));
 
-        paciente.querySelector(".info-imc").textContent = imcPaciente;
+        if (verificacionPeso == true && verificacionAltura) {
+            var imcPaciente = imc(parseFloat(peso), parseFloat(altura));
+            /*console.log(imcPaciente)*/
+            paciente.querySelector(".info-imc").textContent = imcPaciente;
+        } else {
+            alert(nombre + " tiene un dato irreal, favor de ingresar informacion correcta.");
+            if (validationPeso == false) {
+                paciente.querySelector(".info-imc").textContent = "Peso Incorrecto";
+            }
+            if (validationAltura == false) {
+                paciente.querySelector(".info-imc").textContent = "Altura Incorrecta";
+            }
+        }
+        
     }
 }
 
-console.log(getPaciente());
+getPaciente();
